477b66a08e4d2815a19a8f3d44ba02707b96954b
AlvarezJoseManuel-ST <g00463862@atu.ie>
2026-02-04 16:18:58 +0000

    setup project

---
+++ src/main/java/atu/ie/s2lab2/S2Lab2Application.java
@@ -0,0 +1,13 @@
+package atu.ie.s2lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2Lab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2Lab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/s2lab2/S2Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.s2lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2Lab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
