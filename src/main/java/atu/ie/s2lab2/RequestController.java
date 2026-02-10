fcf1fb21ab8b43a430ba4cb70e33b0ca9013c4ea
Jose Alvarez <g00463862@atu.ie>
2026-02-10 10:53:40 +0000

    seetting application app + request for email and password

--- src/main/java/atu/ie/s2lab2/RequestController.java
+++ src/main/java/atu/ie/s2lab2/RequestController.java
@@ -5,25 +5,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 public class RequestController {
-    /* I need to send 2 numbers and select the type of calculation I want to make.
-    * I must receive the result of the operation*/
-    /*Here I'm going to receive the https requests and send the data to the calculator class*/
+    /* I need to send 2 pieces of information.
+    * I need to ask for the user email address and password
+    * Here I'm going to receive the https requests*/
 
-    private Calculator calculator;
-
-    public RequestController(Calculator calculator) {
-        this.calculator = calculator;
+    @GetMapping("/get/{email}")
+    public String getEmail(@PathVariable String email){
+        return email;
     }
-    @GetMapping("/calculate/")
-    public double calculate(
-            @RequestParam double firstNumber,
-            @RequestParam double secondNumber,
-            @RequestParam String operator) {
-        if (operator.equals("+")) return calculator.add(firstNumber, secondNumber);
-        if (operator.equals("-")) return calculator.subtract(firstNumber, secondNumber);
-        if (operator.equals("*")) return calculator.multiply(firstNumber, secondNumber);
-        if (operator.equals("/")) return calculator.divide(firstNumber, secondNumber);
-    throw new IllegalArgumentException("Invalid operator");
+    @GetMapping("/get/{password}")
+    public String getPassword(String password){
+        return password;
     }
 
 }
