f9ab78f87a1430134f2e958acfc385fba0a16541
Jose Alvarez <g00463862@atu.ie>
2026-02-18 20:34:20 +0000

    commiti code

---
+++ src/main/java/atu/ie/s2lab2/service/LogInService.java
@@ -0,0 +1,15 @@
+package atu.ie.s2lab2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogInService {
+
+    public String email(String userEmail){
+    return userEmail;
+    }
+    public String password(String password){
+        return password;
+    }
+
+}
