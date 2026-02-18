e80ec4f875505709eb9dbc3a3b5e9b4cbd04d1b3
OisinATU <G00470518@atu.ie>
2026-02-18 19:09:30 +0000

    Created project

---
+++ src/main/java/ie/atu/java/atu_app/AtuAppApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.java.atu_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtuAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtuAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/java/atu_app/AtuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.java.atu_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtuAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
